a2a82af09e616337326bffbb63d9a07713201e8f
INO Shiori <21716012@jwu.ac.jp>
2019-10-07 16:49:46 +0900

    added gcd

--- rational.py
+++ rational.py
@@ -1,7 +1,10 @@
+import math
+
 class Q(object):
     def __init__(self,a,b=1):
-        self.a = a
-        self.b = b
+        gcd=math.gcd(a,b)
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self):
         if self.b==1:
             return str(self.a)
@@ -12,19 +15,19 @@ class Q(object):
         c=q.a
         d=q.b
         return Q(a*d+b*c,b*d)
-    def __sub__(self, q):
+    #def __sub__(self, q):
         a=self.a
         b=self.b
         c=q.a
         d=q.b
         return Q(a*d-b*c,b*d)
-    def __mul__(self, q):
+    #def __mul__(self, q):
         a=self.a
         b=self.b
         c=q.a
         d=q.b
         return Q(a*c,b*d)
-    def __truediv__(self, q):
+    #def __truediv__(self, q):
         a=self.a
         b=self.b
         c=q.a
@@ -34,6 +37,6 @@ class Q(object):
 q1 = Q(1,2)
 q2 = Q(1,3)
 print(q1 + q2)
-print(q1-q2)
-print(q1*q2)
-print(q1/q2)
+#print(q1-q2)
+#print(q1*q2)
+#print(q1/q2)
